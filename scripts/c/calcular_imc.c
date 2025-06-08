8c93a41c2b11969357052fc697a4e0382d8fc73f
cupitatiana <cupitatiana@gmail.com>
2025-06-08 16:39:25 -0500

    imc con c y goose

---
+++ scripts/c/calcular_imc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Función para calcular el IMC
+float calcularIMC(float peso, float altura) {
+	return peso / (altura * altura);
+}
+
+int main() {
+	float peso, altura;
+
+	// Solicitar datos al usuario
+	printf("Ingresa tu peso en kilogramos: ");
+	scanf("%f", &peso);
+	printf("Ingresa tu altura en metros: ");
+	scanf("%f", &altura);
+
+	float imc = calcularIMC(peso, altura);
+	printf("Tu IMC es: %.2f\n", imc);
+
+	return 0;
+}
